fc1292cd94cd8c02f6679f43aa32df495f75ab97
sxxnxhy <seunghyu823@gmail.com>
2025-03-07 14:43:45 +0900

    update: 프론트 .html 없애기

--- src/main/java/com/application/letschat/controller/MainController.java
+++ src/main/java/com/application/letschat/controller/MainController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+//    @GetMapping("/")
+//    public String home(HttpServletRequest request) {
+//        if (request.getSession().getAttribute("user") == null) {
+//            return "redirect:/login.html";
+//        }
+//        return "redirect:chat-list.html";
+//    }
     @GetMapping("/")
     public String home(HttpServletRequest request) {
         if (request.getSession().getAttribute("user") == null) {
-            return "redirect:/login.html";
+            return "redirect:https://syoo.shop/login"; // Nginx serves this
         }
-        return "redirect:chat-list.html";
+        return "redirect:https://syoo.shop/chat-list"; // Nginx serves this
     }
 }
